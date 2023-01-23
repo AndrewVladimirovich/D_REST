4708faf77dc31f4181f923cde7784b0e5f9499dc
Andrey Tretyakov <andrew1_2002@mail.ru>
2023-01-23 22:11:05 +0300

    Lesson 2 models, serializers, views changes

--- library/app/serializers.py
+++ library/app/serializers.py
@@ -1,9 +1,27 @@
 from rest_framework.serializers import HyperlinkedModelSerializer
-from .models import Author
+from .models import Author, Biography, Article, Book
 
 class AuthorModelSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Author
+        fields = '__all__'
+
+
+class BiographyModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Biography
+        fields = '__all__'
+
+
+class ArticleModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Article
+        fields = '__all__'
+
+
+class BookModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Book
         fields = '__all__'
 
         
