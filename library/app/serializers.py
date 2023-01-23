afa5b9316f2b12eaba6f2b4ae33074fcd675d37a
Tretyakov Andrey <94076226+AndrewVladimirovich@users.noreply.github.com>
2023-01-24 00:00:05 +0300

    Lesson 3 (#3)
    * Lesson 2 models, serializers, views changes
    
    * Lesson 2 next changes
    
    * Lesson 2 next changes1
    
    * Lesson 2 renderers in settings.py

--- library/app/serializers.py
+++ library/app/serializers.py
@@ -1,9 +1,26 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
-from .models import Author
+from rest_framework.serializers import ModelSerializer
+from .models import Author, Biography, Article, Book
 
-class AuthorModelSerializer(HyperlinkedModelSerializer):
+class AuthorModelSerializer(ModelSerializer):
     class Meta:
         model = Author
+        fields = '__all__'
+
+class BiographyModelSerializer(ModelSerializer):
+    class Meta:
+        model = Biography
+        fields = '__all__'
+
+
+class ArticleModelSerializer(ModelSerializer):
+    class Meta:
+        model = Article
+        fields = '__all__'
+
+
+class BookModelSerializer(ModelSerializer):
+    class Meta:
+        model = Book
         fields = '__all__'
 
         
--- library/app/views.py
+++ library/app/views.py
@@ -1,7 +1,22 @@
 from rest_framework.viewsets import ModelViewSet
-from .models import Author
-from .serializers import AuthorModelSerializer
+from .models import Author, Article, Biography, Book
+from .serializers import AuthorModelSerializer, ArticleModelSerializer, BiographyModelSerializer, BookModelSerializer
 
 class AuthorModelViewSet(ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorModelSerializer
+
+
+class ArticleModelViewSet(ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleModelSerializer
+
+
+class BiographyModelViewSet(ModelViewSet):
+    queryset = Biography.objects.all()
+    serializer_class = BiographyModelSerializer
+
+
+class BookModelViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookModelSerializer
