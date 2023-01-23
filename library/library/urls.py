65c9bcdb01338f6acf3d2de8488378e30f4a2e21
Andrey Tretyakov <andrew1_2002@mail.ru>
2023-01-23 22:47:42 +0300

    Lesson 2 next changes

--- library/library/urls.py
+++ library/library/urls.py
@@ -17,13 +17,19 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from app.views import AuthorModelViewSet
+from app.views import AuthorModelViewSet, ArticleModelViewSet, BiographyModelViewSet, BookModelViewSet
+
 
 router = DefaultRouter()
 router.register('authors', AuthorModelViewSet)
+router.register('article', ArticleModelViewSet)
+router.register('biography', BiographyModelViewSet)
+router.register('books', BookModelViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
 ]
 
