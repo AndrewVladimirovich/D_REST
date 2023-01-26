fba09f5a391dd4252c529cb003f38da9f1fdfbe0
Tretyakov Andrey <94076226+AndrewVladimirovich@users.noreply.github.com>
2023-01-27 01:04:06 +0300

    Lesson 4 (#4)
    * Lesson_4 MyAPI create
    
    * Lesson_4 ViewSet
    
    * Lesson_4 ViewSet routers conf
    
    * Lesson_4 ViewSet filters
    
    * Lesson_4 Django filters
    
    * Lesson_4 Pagination

--- library/app/views.py
+++ library/app/views.py
@@ -1,11 +1,18 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, ViewSet
 from .models import Author, Article, Biography, Book
 from .serializers import AuthorModelSerializer, ArticleModelSerializer, BiographyModelSerializer, BookModelSerializer
+from rest_framework.views import APIView
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, DestroyAPIView, UpdateAPIView
+from rest_framework import viewsets
+from rest_framework.decorators import action
+
 
 class AuthorModelViewSet(ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorModelSerializer
-
+    filterset_fields = ['first_name', 'last_name', 'bithday_year']
 
 class ArticleModelViewSet(ModelViewSet):
     queryset = Article.objects.all()
@@ -20,3 +27,22 @@ class BiographyModelViewSet(ModelViewSet):
 class BookModelViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookModelSerializer
+
+
+class MyAPIView(ViewSet):
+   
+    def list(self, request):
+       authors = Author.objects.all()
+       serializer = AuthorModelSerializer(authors, many=True)
+       return Response(serializer.data)
+    
+    def destroy(self, request):
+        authors = Author.objects.all()
+        serializer = AuthorModelSerializer(authors, many=True)
+        return Response(serializer.data)
+
+
+    @action(detail=False, methods=['get'])
+    def babayka(self, request):
+        return Response({'data': 'RATATA'})
+
--- library/library/urls.py
+++ library/library/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from app.views import AuthorModelViewSet, ArticleModelViewSet, BiographyModelViewSet, BookModelViewSet
+from app.views import AuthorModelViewSet, ArticleModelViewSet, BiographyModelViewSet, BookModelViewSet, MyAPIView
 
 
 router = DefaultRouter()
@@ -25,6 +25,7 @@ router.register('authors', AuthorModelViewSet)
 router.register('article', ArticleModelViewSet)
 router.register('biography', BiographyModelViewSet)
 router.register('books', BookModelViewSet)
+router.register('my', MyAPIView, basename='my')
 
 
 urlpatterns = [
