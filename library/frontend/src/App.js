c1dc69eac5aeb4a481dcd6c149a9440fe0295f99
Tretyakov Andrey <94076226+AndrewVladimirovich@users.noreply.github.com>
2023-02-02 18:33:38 +0300

    Lesson 5 (#5)
    * Lesson_5 books add react install
    
    * Lesson_5 routing
    
    * Lesson_5 Switch
    
    * Lesson_5 Redirect
    
    * Lesson_5 AuthorBookList
    
    * Lesson_5 BrowserRouter

--- library/frontend/src/App.js
+++ library/frontend/src/App.js
@@ -2,29 +2,76 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AuthorList from './components/Author.js'
+import BookList from './components/Books' 
+import AuthorBookList from './components/AuthorBook'
+import {HashRouter, Route, Link, Switch, Redirect, BrowserRouter} from 'react-router-dom'
 import axios from 'axios'
+
+const NotFound404 = ({ location }) => {
+    return (
+        <div>
+            <h1>Страница по адресу '{location.pathname}' не найдена</h1>
+        </div>
+    )
+}
+
 
 class App extends React.Component {
    constructor(props) {
        super(props)
+
+       const author1 = {id: 1, first_name: 'Достоевский', birthday_year: 1821}
+       const author2 = {id: 2, first_name: 'Чехов', birthday_year: 1860}
+       const authors = [author1, author2]
+
+       const book1 = {id: 1, name: 'Преступление и наказание', author: author1}
+       const book2 = {id: 2, name: 'Игрок', author: author1}
+       const book3 = {id: 3, name: 'Лошадинная фамилия', author: author2}
+       const book4 = {id: 4, name: 'Пари', author: author2}
+       const books = [book1, book2, book3, book4]
+
+
        this.state = {
-           'authors': []
-} }
-componentDidMount() {
-  axios.get('http://127.0.0.1:8000/api/authors')
-        .then(response => {
-            const authors = response.data
-              this.setState(
-              {
-                  'authors': authors
-              }
-            )
-          }).catch(error => console.log(error))
-}
-   render () {
-       return (
-           <div>
-               <AuthorList authors={this.state.authors} />
+           'authors': authors,
+           'books': books
+        } 
+    }
+
+    
+    // componentDidMount() {
+    // axios.get('http://127.0.0.1:8000/api/authors')
+    //         .then(response => {
+    //             const authors = response.data
+    //             this.setState(
+    //             {
+    //                 'authors': authors
+    //             }
+    //             )
+    //         }).catch(error => console.log(error))
+    // }
+    
+    render () {
+        return (
+            <div className='App'>
+                <BrowserRouter>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to='/'>Authors</Link>                                
+                            </li>
+                            <li>
+                                <Link to='/books'>Books</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <Switch>
+                        <Route exact path='/' component={() => <AuthorList authors={this.state.authors}> </AuthorList>} />
+                        <Route exact path='/books' component={() => <BookList items={this.state.books}> </BookList>} />
+                        <Route exact path='/author/:id' component={() => <AuthorBookList items={this.state.books}> </AuthorBookList>} />
+                        <Redirect from='/authors' to='/'/>
+                        <Route component={NotFound404}/>
+                    </Switch>
+                </BrowserRouter>
             </div>
         )
     } 
--- library/frontend/src/components/Author.js
+++ library/frontend/src/components/Author.js
@@ -1,29 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const AuthorItem = ({author}) => {
    return (
-<tr> <td>
+        <tr> 
+            <td>
+               <Link to={'author/${author.id}'}>{author.id}</Link>
+            </td>
+            <td>
                {author.first_name}
-           </td>
-           <td>
-               {author.last_name}
-</td> <td>
+            </td>
+            <td>
                {author.birthday_year}
-           </td>
-</tr> )
+            </td>
+        </tr> 
+    )
 }
 
 const AuthorList = ({authors}) => {
     return (
- <table> <th>
-                First name
+        <table>
+            <th>
+                ID
             </th>
             <th>
-                Last Name
- </th> <th>
+                First Name
+            </th> 
+            <th>
                 Birthday year
             </th>
             {authors.map((author) => <AuthorItem author={author} />)}
         </table>
  ) }
+
  export default AuthorList
