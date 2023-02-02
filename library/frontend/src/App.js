4545abb2c083499a0df2aa338fe46743f8a53dbf
Andrey Tretyakov <andrew1_2002@mail.ru>
2023-02-02 14:23:43 +0300

    Lesson_5 books add react install

--- library/frontend/src/App.js
+++ library/frontend/src/App.js
@@ -2,29 +2,48 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AuthorList from './components/Author.js'
+import BookList from './components/Books' 
 import axios from 'axios'
 
 class App extends React.Component {
    constructor(props) {
        super(props)
+
+       const author1 = {id: 1, first_name: 'Достоевский', birthday_year: 1821}
+       const author2 = {id: 2, first_name: 'Чехов', birthday_year: 1860}
+       const authors = [author1, author2]
+
+       const book1 = {id: 1, name: 'Преступление и наказание', author: author1}
+       const book2 = {id: 2, name: 'Игрок', author: author1}
+       const book3 = {id: 3, name: 'Лошадинная фамилия', author: author2}
+       const book4 = {id: 4, name: 'Пари', author: author2}
+       const books = [book1, book2, book3, book4]
+
+
        this.state = {
-           'authors': []
-} }
-componentDidMount() {
-  axios.get('http://127.0.0.1:8000/api/authors')
-        .then(response => {
-            const authors = response.data
-              this.setState(
-              {
-                  'authors': authors
-              }
-            )
-          }).catch(error => console.log(error))
-}
-   render () {
-       return (
-           <div>
+           'authors': authors,
+           'books': books
+        } 
+    }
+
+    
+    // componentDidMount() {
+    // axios.get('http://127.0.0.1:8000/api/authors')
+    //         .then(response => {
+    //             const authors = response.data
+    //             this.setState(
+    //             {
+    //                 'authors': authors
+    //             }
+    //             )
+    //         }).catch(error => console.log(error))
+    // }
+    
+    render () {
+        return (
+            <div>
                <AuthorList authors={this.state.authors} />
+               <BookList items={this.state.books} />
             </div>
         )
     } 
--- library/frontend/src/components/Author.js
+++ library/frontend/src/components/Author.js
@@ -2,28 +2,34 @@ import React from 'react'
 
 const AuthorItem = ({author}) => {
    return (
-<tr> <td>
+        <tr> 
+            <td>
+               {author.id}
+            </td>
+            <td>
                {author.first_name}
-           </td>
-           <td>
-               {author.last_name}
-</td> <td>
+            </td>
+            <td>
                {author.birthday_year}
-           </td>
-</tr> )
+            </td>
+        </tr> 
+    )
 }
 
 const AuthorList = ({authors}) => {
     return (
- <table> <th>
-                First name
+        <table>
+            <th>
+                ID
             </th>
             <th>
-                Last Name
- </th> <th>
+                First Name
+            </th> 
+            <th>
                 Birthday year
             </th>
             {authors.map((author) => <AuthorItem author={author} />)}
         </table>
  ) }
+ 
  export default AuthorList
