2b8d1af9efa959ba18e8d6117c476fc0aef5e225
Tretyakov Andrey <94076226+AndrewVladimirovich@users.noreply.github.com>
2023-02-03 16:48:09 +0300

    Lesson 6 (#6)
    * Lesson_6 Auth settings
    
    * Lesson_6 magrations
    
    * Lesson_6 auth set
    
    * Lesson_6 last changes

--- library/library/settings.py
+++ library/library/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'app',
     'corsheaders',
     'django_filters',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -138,8 +139,11 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_FILTER_BACKENDS': [
-        'django_filters.rest_framework.DjangoFilterBackend'
+        'django_filters.rest_framework.DjangoFilterBackend',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2
