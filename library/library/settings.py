9cb401d935b6ef81a609cc031d41e723718a3ad0
Andrey Tretyakov <andrew1_2002@mail.ru>
2023-02-03 16:42:17 +0300

    Lesson_6 last changes

--- library/library/settings.py
+++ library/library/settings.py
@@ -140,10 +140,10 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.BrowsableAPIRenderer',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_FILTER_BACKENDS': [
-        'django_filters.rest_framework.DjangoFilterBackend'
+        'django_filters.rest_framework.DjangoFilterBackend',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2
