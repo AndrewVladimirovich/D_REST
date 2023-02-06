cea6e9e350fe14fffdc92686c03b49c0c7ba5ad2
Andrey Tretyakov <andrew1_2002@mail.ru>
2023-02-06 14:10:18 +0300

    Lesson_7 Auth components

--- library/frontend/src/components/Auth.js
+++ library/frontend/src/components/Auth.js
@@ -8,10 +8,10 @@ class LoginForm extends React.Component {
 
     handleChange(event) {
         this.setState(
-            state: {
+            {
                 [event.target.name]: event.target.value
             }
-        )
+        );
     }
 
     handleSubmit(event) {
@@ -21,15 +21,15 @@ class LoginForm extends React.Component {
 
     render() {
         return (
-        <form onSubmit={(event:FormEvent<HTMLFormElement>) => this.handleSubmit(event)}>
-            <input type="text" name="login" placeholder="login" value={this.state.login} onChange={(event:ChangeEvent<HTMLInputElement>) => this.handleChange(event)}>
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+            <input type="text" name="login" placeholder="login" value={this.state.login} onChange={(event) => this.handleChange(event)}>
             </input>
-            <input type="password" name="password" placeholder="password" value={this.state.password} onChange={(event:ChangeEvent<HTMLInputElement>) => this.handleChange(event)}>
+            <input type="password" name="password" placeholder="password" value={this.state.password} onChange={(event) => this.handleChange(event)}>
             </input>
             <input type="submit" value="Login">
             </input>
         </form>
-        )
+        );
     }
 }
 
