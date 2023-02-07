0b34e0b0cb8283f2a9c3ce7b13967bb83029f4a0
Andrey Tretyakov <andrew1_2002@mail.ru>
2023-02-07 13:24:38 +0300

    Lesson_8 create tests

--- library/app/tests.py
+++ library/app/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+import json
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
+from django.contrib.auth.models import User
+from .views import AuthorModelViewSet
+from .models import Author, Book
 
-# Create your tests here.
+class TestAuthorViewSet(TestCase):
+
+    def test_get_list(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/authors')
+        view = AuthorModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
