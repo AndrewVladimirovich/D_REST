56847feeab7faa0ee428b402f4df77dcd89ae9cb
Andrey Tretyakov <andrew1_2002@mail.ru>
2023-02-08 13:11:54 +0300

    Lesson_8 apitestcase test8&9

--- library/app/tests.py
+++ library/app/tests.py
@@ -3,7 +3,7 @@ import json
 from rest_framework import status
 from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
 from django.contrib.auth.models import User
-from .views import AuthorModelViewSet
+from .views import AuthorModelViewSet, BookModelViewSet
 from .models import Author, Book
 
 class TestAuthorViewSet(TestCase):
@@ -68,3 +68,26 @@ class TestMath(APISimpleTestCase):
         self.assertEqual(math.sqrt(4), 2)
 
     
+class TestBookViewSet(APITestCase):
+
+    def test_get_lists(self):
+        response = self.client.get('/api/books/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_edit_book_admin(self):
+        author = Author.objects.create(first_name='Andrey', last_name='Tretyakov', bithday_year=1979)
+        book = Book.objects.create(name='Книга')
+        book.author.add(author)
+        book.save()
+
+        admin = User.objects.create_superuser('admin', 'admin@admin.com', 'admin')
+        self.client.login(username='admin', password='admin')
+        response = self.client.put(f'/api/books/{book.id}/', {'name': 'Война и мир', 'author': author.id})
+
+        print(response.json())
+        book = Book.objects.get(pk=book.id)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(book.name, 'Война и мир')
+        self.client.logout()
+
+        
