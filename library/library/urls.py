c35686ccc7c1019bac1c34307e0478c38493a0ae
Tretyakov Andrey <94076226+AndrewVladimirovich@users.noreply.github.com>
2023-02-15 19:48:07 +0300

    Lesson 10 (#10)
    * Lesson_10 graphene install & create schema.py
    
    * Lesson_10 schema.py changeing
    
    * Lesson_10 schema.py modeing
    
    * Lesson_10 add more schm
    
    * Lesson_10 mutations

--- library/library/urls.py
+++ library/library/urls.py
@@ -21,6 +21,7 @@ from rest_framework import permissions
 from django.urls import re_path
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from graphene_django.views import GraphQLView
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -49,6 +50,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api-token-auth/', obtain_auth_token),
+    path('graphql/', GraphQLView.as_view(graphiql=True)),
     path('api/', include(router.urls)),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
